cb0b12a016d137e4a16f474e02107d7caf52064c
Sanjeev Midda <110365686+SanjeevMidda@users.noreply.github.com>
2024-06-17 09:49:16 +0100

    Add files via upload
    Uploading source files.

---
+++ src/App.js
@@ -0,0 +1,188 @@
+import "./index.css";
+import { motion } from "framer-motion";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="viewContainer">
+        <motion.div
+          className="block"
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          // viewport={{ amount: "all" }}
+          transition={{ duration: 2 }}
+        ></motion.div>
+
+        <motion.div
+          className="block"
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          // viewport={{ amount: "all" }}
+          transition={{ duration: 2 }}
+        ></motion.div>
+
+        <motion.div
+          className="block"
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          // viewport={{ amount: "all" }}
+          transition={{ duration: 2 }}
+        ></motion.div>
+
+        <motion.div
+          className="block"
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          // viewport={{ amount: "all" }}
+          transition={{ duration: 2 }}
+        ></motion.div>
+
+        <motion.div
+          className="block"
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          // viewport={{ amount: "all" }}
+          transition={{ duration: 2 }}
+        ></motion.div>
+
+        <motion.div
+          className="block"
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          // viewport={{ amount: "all" }}
+          transition={{ duration: 2 }}
+        ></motion.div>
+
+        <motion.div
+          className="block"
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          // viewport={{ amount: "all" }}
+          transition={{ duration: 2 }}
+        ></motion.div>
+
+        <motion.div
+          className="block"
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          // viewport={{ amount: "all" }}
+          transition={{ duration: 2 }}
+        ></motion.div>
+
+        <motion.div
+          className="block"
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          // viewport={{ amount: "all" }}
+          transition={{ duration: 2 }}
+        ></motion.div>
+
+        <motion.div
+          className="block"
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          // viewport={{ amount: "all" }}
+          transition={{ duration: 2 }}
+        ></motion.div>
+
+        <motion.div
+          className="block"
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          // viewport={{ amount: "all" }}
+          transition={{ duration: 2 }}
+        ></motion.div>
+
+        <motion.div
+          className="block"
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          // viewport={{ amount: "all" }}
+          transition={{ duration: 2 }}
+        ></motion.div>
+
+        <motion.div
+          className="block"
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          // viewport={{ amount: "all" }}
+          transition={{ duration: 2 }}
+        ></motion.div>
+
+        <motion.div
+          className="block"
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          // viewport={{ amount: "all" }}
+          transition={{ duration: 2 }}
+        ></motion.div>
+
+        <motion.div
+          className="block"
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          // viewport={{ amount: "all" }}
+          transition={{ duration: 2 }}
+        ></motion.div>
+
+        <motion.div
+          className="block"
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          // viewport={{ amount: "all" }}
+          transition={{ duration: 2 }}
+        ></motion.div>
+
+        <motion.div
+          className="block"
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          // viewport={{ amount: "all" }}
+          transition={{ duration: 2 }}
+        ></motion.div>
+
+        <motion.div
+          className="block"
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          // viewport={{ amount: "all" }}
+          transition={{ duration: 2 }}
+        ></motion.div>
+
+        <motion.div
+          className="block"
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          // viewport={{ amount: "all" }}
+          transition={{ duration: 2 }}
+        ></motion.div>
+
+        <motion.div
+          className="block"
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          // viewport={{ amount: "all" }}
+          transition={{ duration: 2 }}
+        ></motion.div>
+
+        <motion.div
+          className="block"
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          // viewport={{ amount: "all" }}
+          transition={{ duration: 2 }}
+        ></motion.div>
+
+        <motion.div
+          className="block"
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          // viewport={{ amount: "all" }}
+          transition={{ duration: 2 }}
+        ></motion.div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
